f68da275877ee6a99f47968dafb6df49d3b7f374
Jacob David C. Cunningham <jcun4128@gmail.com>
2022-01-11 18:12:36 -0600

    make base wider

--- robot/code/motion/gaits/gaits.ino
+++ robot/code/motion/gaits/gaits.ino
@@ -26,6 +26,48 @@ Servo backLeftOuterServo;
 Servo backLeftMiddleServo;
 Servo backLeftInnerServo;
 
+bool motionInProgress = false;
+
+Servo getServoByPin(int digitalPin)
+{
+  if (digitalPin == 0) // ugh this sucks
+  {
+    return frontRightInnerServo;
+  } else if (digitalPin == 1)
+  {
+    return frontRightMiddleServo;
+  } else if (digitalPin == 2)
+  {
+    return frontRightOuterServo;
+  } else if (digitalPin == 3)
+  {
+    return backRightInnerServo;
+  } else if (digitalPin == 4)
+  {
+    return backRightMiddleServo;
+  } else if (digitalPin == 5)
+  {
+    return backRightOuterServo;
+  } else if (digitalPin == 6)
+  {
+    return frontLeftInnerServo;
+  } else if (digitalPin == 7)
+  {
+    return frontLeftMiddleServo;
+  } else if (digitalPin == 8)
+  {
+    return frontLeftOuterServo;
+  } else if (digitalPin == 9)
+  {
+    return backLeftInnerServo;
+  } else if (digitalPin == 10)
+  {
+    return backLeftMiddleServo;
+  } else {
+    return backLeftOuterServo;
+  }
+}
+
 /**
  * these are attached with regard to the board's design
  * where the right half of the robot pins are on the right side, etc...
@@ -64,27 +106,92 @@ void setAndCenterServos()
 
   // neutral stance
   frontRightInnerServo.write(130); // +40
-  frontRightMiddleServo.write(80); // -10
+  frontRightMiddleServo.write(80); // -10 // motion 40
   frontRightOuterServo.write(95); // +5
-  backRightInnerServo.write(60); // -30
-  backRightMiddleServo.write(100); // +10
+  backRightInnerServo.write(50); // -30
+  backRightMiddleServo.write(100); // +10 // motion 75
   backRightOuterServo.write(75); // -15
-  frontLeftInnerServo.write(50); // -30
+  frontLeftInnerServo.write(40); // -30
   frontLeftMiddleServo.write(100); // +10
   frontLeftOuterServo.write(85); // -5
-  backLeftInnerServo.write(120); // +30
+  backLeftInnerServo.write(130); // +30
   backLeftMiddleServo.write(80); // -10
   backLeftOuterServo.write(95); // +5
 }
 
+// ex call
+// servoGroupArr[][3] = {
+//   {#, #, #}
+//   {#, #, #}
+//   {#, #, #}
+//   {#, #, #}    
+// }
+
+/**
+ * This function moves servos together at the same speed
+ * there is a "bias" where the shortest motion will end first
+ * @param {int} - array of arrays with the following: servo enum, start pos, end pos
+ * @param {int} - array length
+ * @param {int} - duration of motion
+ */
+void moveServos(int servoGroupArr[][3], int servoGroupArrLen, int motionDuration) {
+  /**
+   * loop through servos, find largest range between start/end pos
+   * there is no error checking, I find coding in Arduino to be cumbersome */
+  motionInProgress = true;
+  int largestServoRange = 0;
+
+  for (int servoGroupIndex = 0; servoGroupIndex < servoGroupArrLen; servoGroupIndex++) {
+    int range = 0;
+    if (servoGroupArr[servoGroupIndex][1] < servoGroupArr[servoGroupIndex][2]) {
+      // increase
+      range = servoGroupArr[servoGroupIndex][2] - servoGroupArr[servoGroupIndex][1];
+      if (range > largestServoRange) {
+        largestServoRange = range;
+      }
+    } else {
+      // decrease
+      range = servoGroupArr[servoGroupIndex][1] - servoGroupArr[servoGroupIndex][2];
+      if (range > largestServoRange) {
+        largestServoRange = range;
+      }
+    }
+  }
+
+  /* do the motion on the servos going by largest range
+   * this will update all the servo positions at the same rate
+   * but some will finish earlier than others if the motion range is not the same */
+  for (int pos = 0; pos < largestServoRange; pos++) {
+    for(int servoGroupIndex = 0; servoGroupIndex < servoGroupArrLen; servoGroupIndex++) {
+      if (servoGroupArr[servoGroupIndex][1] < servoGroupArr[servoGroupIndex][2]) {
+        // increase
+        if (servoGroupArr[servoGroupIndex][1] + pos < servoGroupArr[servoGroupIndex][2]) {
+          getServoByPin(servoGroupArr[servoGroupIndex][0]).write(servoGroupArr[servoGroupIndex][1] + pos);
+        }
+      } else {
+        // decrease
+        if (servoGroupArr[servoGroupIndex][1] - pos > servoGroupArr[servoGroupIndex][2]) {
+          getServoByPin(servoGroupArr[servoGroupIndex][0]).write(servoGroupArr[servoGroupIndex][1] - pos);
+        }
+      }
+    }
+    delay(motionDuration);
+  }
+
+  motionInProgress = false;
+}
+
 void moveForward()
 {
+  // int servoGroupArr[][3] = {
 
+  // }
 }
 
 void setup()
 {
   setAndCenterServos();
+  // moveForward();
 }
 
 void loop()
