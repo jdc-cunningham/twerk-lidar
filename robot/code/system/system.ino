f807f6f45164b83172e16e1437360105422110b7
Jacob David C. Cunningham <jcun4128@gmail.com>
2022-01-26 14:27:49 -0600

    data being sent by ESP01

--- robot/code/system/system.ino
+++ robot/code/system/system.ino
@@ -44,17 +44,15 @@ void loop()
   // turnLeft();
   if (imu.Read()) {
     // Serial5.print(sensor.readRangeSingleMillimeters() * 0.0393701);
-    // checkEspSerialMsg();
+    checkEspSerialMsg();
 
-    Serial5.println("esp");
+    String sensorAccelSample = String(imu.accel_x_mps2()) + "," + String(imu.accel_y_mps2()) + "," + String(imu.accel_z_mps2()) + ",";
+    String sensorGyroSample = String(imu.gyro_x_radps()) + "," + String(imu.gyro_y_radps()) + "," + String(imu.gyro_z_radps()) + ",";
+    String sensorMagSample = String(imu.mag_x_ut()) + "," + String(imu.mag_y_ut()) + "," + String(imu.mag_z_ut());
 
-    // String sensorAccelSample = String(imu.accel_x_mps2()) + "," + String(imu.accel_y_mps2()) + "," + String(imu.accel_z_mps2());
-    // String sensorGyroSample = String(imu.gyro_x_radps()) + "," + String(imu.gyro_y_radps()) + "," + String(imu.gyro_z_radps());
-    // String sensorMagSample = String(imu.mag_x_ut()) + "," + String(imu.mag_y_ut()) + "," + String(imu.mag_z_ut());
-
-    // Serial5.println(sensorAccelSample);
-    // Serial5.println(sensorGyroSample);
-    // Serial5.println(sensorMagSample);
+    Serial5.print(sensorAccelSample);
+    Serial5.print(sensorGyroSample);
+    Serial5.print(sensorMagSample);
   }
 
   delay(1000);
