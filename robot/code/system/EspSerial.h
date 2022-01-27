574a204040f8b8fbbb0e9f0bfd309fe2235c6dae
Jacob David C. Cunningham <jcun4128@gmail.com>
2022-01-27 13:52:34 -0600

    sadness

--- robot/code/system/EspSerial.h
+++ robot/code/system/EspSerial.h
@@ -15,7 +15,6 @@ void clearEspSerial()
 String getEspSerialMsg() {
   if (ESPSERIAL.available() > 0) {
     String msg = ESPSERIAL.readString();
-    clearEspSerial(); // might be redundant
     return msg;
   }
 
--- robot/code/system/system.ino
+++ robot/code/system/system.ino
@@ -24,16 +24,17 @@
 void setup()
 {
   setAndCenterServos();
-  // Serial.begin(115200);
   setupEspSerial();
-  // Serial.println(getBatteryVoltage());
   setupTof();
   setupImu();
-  // Serial.println(sensor.readRangeSingleMillimeters() * 0.0393701);
-  delay(3000);
+  // delay(3000);
+
   // turnLeft();
   // performScan();
   // Serial.print(sampleSetPerSweep[0][0]);
+  // Serial.begin(115200);
+  // Serial.println(getBatteryVoltage());
+  // Serial.println(sensor.readRangeSingleMillimeters() * 0.0393701);
 }
 
 void loop()
@@ -41,22 +42,32 @@ void loop()
   // moveForward3();
   // performScan();
   // turnLeft();
-  if (imu.Read()) {
-    // Serial5.print(sensor.readRangeSingleMillimeters() * 0.0393701);
-    String espMsg = getEspSerialMsg();
+  // if (imu.Read()) {
+  //   // Serial5.print(sensor.readRangeSingleMillimeters() * 0.0393701);
+    // String espMsg = getEspSerialMsg();
 
-    if (espMsg.length() > 0)
-    {
-      String sensorAccelSample = String(imu.accel_x_mps2()) + "," + String(imu.accel_y_mps2()) + "," + String(imu.accel_z_mps2()) + ",";
-      String sensorGyroSample = String(imu.gyro_x_radps()) + "," + String(imu.gyro_y_radps()) + "," + String(imu.gyro_z_radps()) + ",";
-      String sensorMagSample = String(imu.mag_x_ut()) + "," + String(imu.mag_y_ut()) + "," + String(imu.mag_z_ut());
+  //   if (espMsg.length() > 0)
+  //   {
+  //     clearEspSerial(); // might be redundant from readString
+  //     moveForward3();
+      // ESPSERIAL.print("esp");
 
-      ESPSERIAL.print(sensorAccelSample);
-      ESPSERIAL.print(sensorGyroSample);
-      ESPSERIAL.print(sensorMagSample);
-      clearEspSerial();
-    }
-  }
+      // String sensorAccelSample = String(imu.accel_x_mps2()) + "," + String(imu.accel_y_mps2()) + "," + String(imu.accel_z_mps2()) + ",";
+      // String sensorGyroSample = String(imu.gyro_x_radps()) + "," + String(imu.gyro_y_radps()) + "," + String(imu.gyro_z_radps()) + ",";
+      // String sensorMagSample = String(imu.mag_x_ut()) + "," + String(imu.mag_y_ut()) + "," + String(imu.mag_z_ut());
+
+      // ESPSERIAL.print(sensorAccelSample);
+      // ESPSERIAL.print(sensorGyroSample);
+      // ESPSERIAL.print(sensorMagSample);
+
+
+      // clearEspSerial();
+  //   }
+  // }
+
+  ESPSERIAL.print("esp");
+
+  // moveForward3();
 
   delay(1000);
 }
