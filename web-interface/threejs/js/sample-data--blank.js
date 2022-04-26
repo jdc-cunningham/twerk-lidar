4a002a4c19a5214c4e60c1c78875a14089c351ff
Jacob Cunningham <jcun4128@gmail.com>
2022-04-26 14:36:34 -0500

    parsing sample

---
+++ web-interface/threejs/js/sample-data--blank.js
@@ -0,0 +1,30 @@
+const sensorSamples = {
+  "tilt-up-2": {
+    "angle": 15.3,
+    "trim": 'beginning',
+    "tof-samples": ``,
+    "sweep-angles": ``
+  },
+  "tilt-up-1": {
+    "angle": 7.2,
+    "tof-samples": ``,
+    "sweep-angles": ``
+  },
+  "level": {
+    "angle": 0,
+    "trim": 'end',
+    "tof-samples": ``,
+    "sweep-angles": ``
+  },
+  "tilt-down-1": {
+    "angle": -6.8,
+    "trim": 'beginning',
+    "tof-samples": ``,
+    "sweep-angles": ``
+  },
+  "tilt-down-2": {
+    "angle": -16.1,
+    "tof-samples": ``,
+    "sweep-angles": ``
+  }
+};
