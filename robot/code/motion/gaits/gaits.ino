26afe5b5c86ee3d71047785b20aa6b8a90cc7790
Jacob David C. Cunningham <jcun4128@gmail.com>
2022-07-03 11:42:47 -0500

    slash and burn

--- robot/code/motion/gaits/gaits.ino
+++
@@ -1,1 +0,0 @@
-// move stuff in here
--- robot/code/system/ESP01.h
+++
@@ -1,9 +0,0 @@
-String getEspSerialMsg() {
-  if (ESPSERIAL.available() > 0) {
-    String msg = ESPSERIAL.readString();
-    clearEspSerial(); // might be redundant
-    return msg;
-  }
-
-  return "";
-}
