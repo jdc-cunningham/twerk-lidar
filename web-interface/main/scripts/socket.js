0a5ab0cc9136a1ec3cf0393b1a6481bf905444a9
Jacob Cunningham <jcun4128@gmail.com>
2022-07-11 21:16:51 -0500

    added messaging

--- web-interface/main/scripts/socket.js
+++ web-interface/main/scripts/socket.js
@@ -4,19 +4,21 @@ const socket = new WebSocket('ws://192.168.1.187:80'); // esp01 on robot
 
  // connection opened, send messages to robot
  socket.addEventListener('open', function (event) {
-  console.log('connected');
   socket.send('Hello Server!');
+  sentMsg('Hello Server!');
   setRobotConnected(true);
 
   // keep connection to esp01 alive
   setInterval(() => {
     socket.send('poll');
+    sentMsg('poll');
   }, 1000);
 });
 
 // listen for messages from robot
 socket.addEventListener('message', function (event) {
   const robotMsg = event.data;
+  receivedMsg(robotMsg);
 
   batteryVoltage(robotMsg);
 });
