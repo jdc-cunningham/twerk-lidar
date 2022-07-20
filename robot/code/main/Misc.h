1ff959ad3b0d59f97d7bcf165244510f8431b6b3
Jacob David C. Cunningham <jcun4128@gmail.com>
2022-07-19 20:30:02 -0500

    starting to work on batch data send

--- robot/code/main/Misc.h
+++ robot/code/main/Misc.h
@@ -1,56 +1,60 @@
-void dumpData()
+void dumpData(bool useSerial)
 {
-  Serial.println(gyroVals.size());
+  if (useSerial) Serial.println(gyroVals.size());
 
   // https://stackoverflow.com/a/14070977/2710227
   // sucks but easier to copy-paste into excel
-  Serial.println("s");
+  if (useSerial) Serial.println("s");
   for (auto it = servoPosVals.cbegin(); it != servoPosVals.cend(); ++it)
   {
-    Serial.println(it->first);
+    if (useSerial) Serial.println(it->first);
   }
 
   for (auto it = servoPosVals.cbegin(); it != servoPosVals.cend(); ++it)
   {
-    Serial.println(it->second);
+    if (useSerial) Serial.println(it->second);
   }
 
-  Serial.println("g");
+  // Serial.println("g");
   for (auto it = gyroVals.cbegin(); it != gyroVals.cend(); ++it)
   {
-    Serial.println(it->first);
+    if (useSerial) Serial.println(it->first);
   }
 
   for (auto it = gyroVals.cbegin(); it != gyroVals.cend(); ++it)
   {
-    Serial.println(it->second);
+    if (useSerial) Serial.println(it->second);
   }
 
-  Serial.println("d");
+  if (useSerial) Serial.println("d");
   for (auto it = depthVals.cbegin(); it != depthVals.cend(); ++it)
   {
-    Serial.println(it->first);
+    if (useSerial) Serial.println(it->first);
   }
 
   for (auto it = depthVals.cbegin(); it != depthVals.cend(); ++it)
   {
-    Serial.println(it->second);
+    if (useSerial) Serial.println(it->second);
   }
 
   Serial.println("aY");
   for (auto it = yAccelVals.cbegin(); it != yAccelVals.cend(); ++it)
   {
-    Serial.println(it->first);
+    if (useSerial) Serial.println(it->first);
   }
 
   for (auto it = yAccelVals.cbegin(); it != yAccelVals.cend(); ++it)
   {
-    Serial.println(it->second);
+    if (useSerial) Serial.println(it->second);
   }
 
-  gyroVals = {};
-  servoPosVals = {};
-  depthVals = {};
-  yAccelVals = {};
-  xAccelVals = {};
+  // clear here otherwise transmit using batch messaging
+  if (useSerial)
+  {
+    gyroVals = {};
+    servoPosVals = {};
+    depthVals = {};
+    yAccelVals = {};
+    xAccelVals = {};
+  }
 }
